cc54281955719fd3406f95f39b17159c296bd8ae
shivam00 <shivam.gupta0002@gmail.com>
2018-05-02 17:14:31 +0530

    notification Api

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n\n\"to\": ".$token.",\n\"title\": ".$title.",\n\"body\": ".$body.",\n\"sound\": \"default\",\n\n\"badge\": 0\n}",
+  CURLOPT_POSTFIELDS => "{\n\n\"to\": \"".$token."\",\n\"title\": \"".$title."\",\n\"body\": \"".$body."\",\n\"sound\": \"default\",\n\n\"badge\": 0\n}",
   CURLOPT_HTTPHEADER => array(
     "Accept: application/json",
     "Content-Type: application/json"
